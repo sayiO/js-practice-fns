1a799e58366ab6bff2fc928151d2071c70360dd5
sayiO <sayionias@gmail.com>
2021-07-24 12:06:37 +0200

    solved the charAt function

--- charAt.js
+++ charAt.js
@@ -1,4 +1,9 @@
 // add whatever parameters you deem necessary - good luck!
-function charAt(){
+function charAt(str, num) {
+    if (num < str.length) {
+        return str[num];
+    } else {
+        return "";
+    }
 
 }
