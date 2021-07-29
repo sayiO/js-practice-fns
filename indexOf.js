9b54654e61d8da6c4a5cda774e4a5e96d5721bc6
sayiO <sayionias@gmail.com>
2021-07-29 12:41:32 +0200

    solved the indexOf function

--- indexOf.js
+++ indexOf.js
@@ -1,4 +1,10 @@
 // add whatever parameters you deem necessary - good luck!
-function indexOf(str, char) {
+function indexOf(arr, num) {
+    for (let i = 0; i < arr.length; i++) {
+        if (num == arr[i]) {
+            return i;
+        }
+    }
 
+    return -1;
 }
