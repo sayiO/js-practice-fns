f01d0fccfa060803718a57a7cf4b0aaae2ec710c
sayiO <sayionias@gmail.com>
2021-07-29 13:34:34 +0200

    solved the min and max array functions

--- max.js
+++ max.js
@@ -1,4 +1,11 @@
 // add whatever parameters you deem necessary - good luck!
-function max(){
+function max(arr) {
+    let max1 = 0;
+    for (let i = 0; i < arr.length; i++) {
 
+        if (max1 < arr[i]) {
+            max1 = arr[i];
+        }
+    }
+    return max1;
 }
--- min.js
+++ min.js
@@ -1,4 +1,11 @@
 // add whatever parameters you deem necessary - good luck!
-function min(){
+function min(arr) {
+    let minimum = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < minimum) {
+            minimum = arr[i];
+        }
+    }
+    return minimum;
 
 }
