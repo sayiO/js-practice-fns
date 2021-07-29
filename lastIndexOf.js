128cd9aef884e48d7847d68fc0b3a933bb94c388
sayiO <sayionias@gmail.com>
2021-07-29 13:05:17 +0200

    solved the lastIndexOf.js function

--- lastIndexOf.js
+++ lastIndexOf.js
@@ -1,4 +1,12 @@
 // add whatever parameters you deem necessary - good luck!
-function lastIndexOf(){
+function lastIndexOf(arr, num) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] === num) {
+            return i;
+
+        }
+
+    }
+    return -1;
 
 }
