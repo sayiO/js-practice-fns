980d182f223718e4b5a7b663da454e6aff9e0626
sayiO <sayionias@gmail.com>
2021-07-30 11:50:06 +0200

    solved the min and max array functions

--- slice.js
+++ slice.js
@@ -1,4 +1,15 @@
 // add whatever parameters you deem necessary - good luck!
-function slice(){
+function slice(arr, start, end) {
+    let arr1 = [];
+    if (end === undefined || end > arr.length) {
+        end = arr.length;
+
+    }
+    for (let i = start; i < end; i++) {
+        arr1.push(arr[i]);
+
+    }
+
+    return arr1;
 
 }
